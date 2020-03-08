d8731e0c78a0c98c5a2d15a839b29c5210d20534
FernandoSorto007 <fernandosorto4@gmail.com>
2020-03-08 02:52:39 +0100

    primer commit

---
+++ Default.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="asp2.Default" %>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <h1 class="text-center">Pagina 2 con ASP</h1>
+     <asp:GridView ID="GridView1" runat="server" Width="675px"></asp:GridView> 
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace asp2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        crud gc = new crud();
+        public void mostrardatos()
+        {
+            gc.LlenarGrid("select * from docentes", "docentes");
+            GridView1.DataSource = gc.ds.Tables["docentes"];
+            GridView1.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            mostrardatos();
+        }
+
+    }
+}
